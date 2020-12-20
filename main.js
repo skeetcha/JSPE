a8d946da0da039a54342917f30ae54d26fe4979b
Daniel Unterholzner <pianojammer@comcast.net>
2020-12-19 18:21:42 -0800

    Working on the menubar some

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -9,8 +9,194 @@ function createWindow() {
         }
     });
 
-    win.loadFile('index.html');
+    win.loadFile('main.html');
 }
+
+function newProject() {
+
+}
+
+function openProject() {
+
+}
+
+function saveProject() {
+
+}
+
+function saveProjectAs() {
+
+}
+
+function exportRom() {
+    
+}
+
+function exportRomAs() {
+
+}
+
+function makePatch() {
+
+}
+
+function openTextEditor() {
+
+}
+
+function openPokemonEditor() {
+
+}
+
+function openMoveEditor() {
+
+}
+
+const isMac = process.platform === 'darwin';
+
+const template = isMac ? [
+    {
+        label: app.name,
+        submenu: [
+            {
+                role: 'about'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'hide'
+            },
+            {
+                role: 'unhide'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'quit'
+            }
+        ]
+    },
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Project',
+                click: newProject,
+                accelerator: 'CmdOrCtrl+N'
+            },
+            {
+                label: 'Open Project',
+                click: openProject,
+                accelerator: 'CmdOrCtrl+O'
+            },
+            {
+                label: 'Save Project',
+                click: saveProject,
+                accelerator: 'CmdOrCtrl+S'
+            },
+            {
+                label: 'Save Project As',
+                click: saveProjectAs,
+                accelerator: 'CmdOrCtrl+Shift+S'
+            },
+            {
+                label: 'Export ROM',
+                click: exportRom
+            },
+            {
+                label: 'Export ROM As',
+                click: exportRomAs
+            },
+            {
+                label: 'Make Patch',
+                click: makePatch
+            },
+            {
+                role: 'close'
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Text Editor',
+                click: openTextEditor
+            },
+            {
+                label: 'Pokemon Editor',
+                click: openPokemonEditor
+            },
+            {
+                label: 'Move Editor',
+                click: openMoveEditor
+            }
+        ]
+    }
+] : [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Project',
+                click: newProject,
+                accelerator: 'CmdOrCtrl+N'
+            },
+            {
+                label: 'Open Project',
+                click: openProject,
+                accelerator: 'CmdOrCtrl+O'
+            },
+            {
+                label: 'Save Project',
+                click: saveProject,
+                accelerator: 'CmdOrCtrl+S'
+            },
+            {
+                label: 'Save Project As',
+                click: saveProjectAs,
+                accelerator: 'CmdOrCtrl+Shift+S'
+            },
+            {
+                label: 'Export ROM',
+                click: exportRom
+            },
+            {
+                label: 'Export ROM As',
+                click: exportRomAs
+            },
+            {
+                label: 'Make Patch',
+                click: makePatch
+            },
+            {
+                role: 'quit'
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Text Editor',
+                click: openTextEditor
+            },
+            {
+                label: 'Pokemon Editor',
+                click: openPokemonEditor
+            },
+            {
+                label: 'Move Editor',
+                click: openMoveEditor
+            }
+        ]
+    }
+]
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
 
 app.whenReady().then(createWindow);
 
